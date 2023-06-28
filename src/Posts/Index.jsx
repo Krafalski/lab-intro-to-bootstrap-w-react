931e0b5ccda5851d9c5fe01979ada537432471cc
K. Rafalski <kr2174@caa.columbia.edu>
2023-06-28 17:49:59 -0400

    Initialize solution

---
+++ src/Posts/Index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import TableRows from "./TableRows";
+import Post from "./Post";
+
+function PostsIndex({ search, posts }) {
+  const postElements = posts.filtered.map((post) => (
+    <Post key={post.id} post={post} />
+  ));
+  const rows = TableRows(posts.all);
+  return (
+    <section className="container">
+      <section className="row row-gap-4">
+        <div className="col-lg-9">
+          <div className="row row-cols-1 row-cols-lg-2 g-lg-3 g-2">
+            {postElements}
+          </div>
+          {!posts.filtered.length && (
+            <div className="alert alert-danger mt-4" role="alert">
+              No posts found with the search term "{search}". Please try again.
+            </div>
+          )}
+        </div>
+        <div className="col-lg-3">
+          <h3>
+            Posts by <span className="text-warning">location</span>
+          </h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Location</th>
+                <th className="text-center" scope="col">
+                  Posts
+                </th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default PostsIndex;
---
+++ src/Search.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Search({ search, handleSearch }) {
+  const handleChange = (event) => {
+    handleSearch(event.target.value);
+  };
+  return (
+    <div className="container my-4 d-none d-lg-block">
+      <form>
+        <div className="row">
+          <div className="col-auto">
+            <label htmlFor="search" className="col-form-label">
+              Search posts by location...
+            </label>
+          </div>
+          <div className="col">
+            <input
+              onChange={handleChange}
+              type="text"
+              id="search"
+              className="form-control"
+              value={search}
+            />
+          </div>
+          <div className="col-auto">
+            <button type="submit" className="btn btn-warning">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Search;
