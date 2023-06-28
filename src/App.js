9204be6668072d57961f712cab6612d0a77f06fc
K. Rafalski <kr2174@caa.columbia.edu>
2023-06-28 17:49:59 -0400

    Initialize solution-cra

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+import Nav from "./Nav";
+import Hero from "./Hero";
+import Search from "./Search";
+import Footer from "./Footer";
+
+import PostsIndex from "./Posts/Index";
+
+import postData from "./data/posts.json";
+
+function App() {
+  const [posts, setPosts] = useState({ all: postData, filtered: postData });
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const term = search.toLowerCase();
+    const filtered = postData.filter((post) => {
+      return (
+        post.location.toLowerCase().includes(term) ||
+        post.title.toLowerCase().includes(term)
+      );
+    });
+
+    if (filtered.length !== posts.filtered.length) {
+      setPosts({ ...posts, filtered });
+    }
+  }, [posts, search]);
+
+  return (
+    <main>
+      <Nav />
+      <Hero />
+      <Search search={search} handleSearch={setSearch} />
+      <PostsIndex search={search} posts={posts} />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Posts/Post.js
@@ -0,0 +1,27 @@
+import images from "../images/index.js";
+
+function Post({ post }) {
+  const dasherized = post.location.split(" ").join("-").toLowerCase();
+
+  return (
+    <div className="col">
+      <div className="card shadow-sm">
+        <img
+          src={images[dasherized]}
+          className="card-img-top img-fluid object-fit-cover post-image-height"
+          alt={post.location}
+        />
+        <div className="card-body p-4">
+          <h5 className="card-title">{post.title}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{post.location}</h6>
+          <p className="text-truncate">{post.content}</p>
+          <div className="text-end">
+            <a className="btn btn-warning">Go to post ➤</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
